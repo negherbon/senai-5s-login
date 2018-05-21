eed76d898cca5c01602222dc2810ee703f7ff4fe
Guilherme Nass <nassguilherme@gmail.com>
2018-05-21 20:22:36 -0300

    set heroku settings in ajax request

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ $('.form-login').submit(function( event ){
     $.ajax({
         method: 'post',
         data: user,
-        url: 'http://localhost:4000/authenticate',
+        url: 'https://api-senai5s.herokuapp.com/authenticate',
         success(data){
             if(data.isAuth)
-                window.location.href = 'http://localhost:4200/auth?token=' + data.token;
+                window.location.href = 'https://web-senai5s.herokuapp.com/auth?token=' + data.token;
         },
         error(data){
             if(data.status === 401)
