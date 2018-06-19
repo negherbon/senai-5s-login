7eee26d44d7327a3ed6d34c2df808b06538555e6
André Felipe <andrefelipe.ml@hotmail.com>
2018-06-19 19:40:32 -0300

    Implementando função de primeiro acesso

---
+++ firstAccess.js
@@ -0,0 +1,27 @@
+$('.form-login').submit(function( event ){
+    event.preventDefault();
+
+    var email = $('.email').val();
+    var cbFirstAccess = document.getElementById("cbFirstAccess").checked
+
+    var password = $('.password').val();
+
+    var user = {
+        email: email,
+        password: password,
+    }
+
+    $.ajax({
+        method: 'post',
+        data: user,
+        url: 'http://localhost:4000/firstAccess',
+        success(data){
+            if(data.isAuth)
+                //redirecionar para componente que seta o token no localstorage
+                window.location.href = 'http://localhost:4200/auth?token=' + data.token;
+        },
+        error(data){
+            alert(data.responseText);
+        }
+    });
+});
