501d5174ec93c7ff449ecedb0d9730c88fc23488
André Felipe <andrefelipe.ml@hotmail.com>
2018-06-25 19:33:07 -0300

    Incluido mensagem de retorno e ajuste no nome dos arquivos

--- first-access.js
+++ first-access.js
@@ -1,11 +1,14 @@
-$('.form-firstAccess').submit(function( event ){
+$('.form-first-access').submit(function( event ){
     event.preventDefault();
 
     let params = new URLSearchParams(document.location.search.substring(1));
     let id = params.get('id');
 
     var password = $('.password').val();
-
+    if($(".password").val() != $(".password-confirm").val()){
+        alert("As senhas não conferem!");
+        return false;
+    }
     var user = {
         id: id,
         password: password,
@@ -15,11 +18,14 @@ $('.form-firstAccess').submit(function( event ){
         data: user,
         url: 'http://localhost:4000/firstAccess',
         success(data){
-            if(data)
+            swal("", data.message, data.type)
+                .then((value) => {
                 window.location.href = 'http://localhost:8080';
+            });
+            //swal("", data.message, data.type),
         },
         error(data){
-            alert(data.responseText);
+            swal("", data.message, data.type);
         }
     });
 });
